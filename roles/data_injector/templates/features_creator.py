01fd258864566f45990beadbf74a304bd4fcf0f6
robinchampseix <robin.champseix@gmail.com>
2018-11-30 15:38:15 +0100

    modified energy creation script

--- roles/data_injector/templates/features_creator.py
+++ roles/data_injector/templates/features_creator.py
@@ -2,9 +2,9 @@
 # coding: utf-8
 """TODO"""
 
-import os
 import configparser
 from typing import List
+import math
 import pandas as pd
 import numpy as np
 from influxdb import InfluxDBClient
@@ -14,6 +14,8 @@ from influxdb import DataFrameClient
 TYPE_PARAM_NAME = "type"
 USER_PARAM_NAME = "user"
 DEVICE_PARAM_NAME = "device_address"
+
+ACCELEROMETER_MEASUREMENT_NAME = "MotionAccelerometer"
 
 
 def get_user_list(client, measurment: str = "RrInterval") -> List[str]:
@@ -31,17 +33,17 @@ def get_user_list(client, measurment: str = "RrInterval") -> List[str]:
     """
     # Get list of all users
     influx_query = "SHOW TAG VALUES WITH KEY = \"user\""
-    result = client.query(influx_query)
+    query_result = client.query(influx_query)
+    user_values_dict = list(query_result.get_points(measurement=measurment))
 
     usr_list = []
-    points = list(result.get_points(measurement=measurment))
-    for elt in points:
+    for elt in user_values_dict:
         usr_list.append(elt["value"])
 
     return usr_list
 
 
-def transform_acm_result_set_into_dataframe(result_set, measurement: str, tags: dict) -> pd.DataFrame:
+def transform_acm_result_set_into_dataframe(result_set: str, tags: dict) -> pd.DataFrame:
     """
     TODO
 
@@ -50,7 +52,7 @@ def transform_acm_result_set_into_dataframe(result_set, measurement: str, tags: 
     :param tags:
     :return:
     """
-    raw_acm_data_list = list(result_set.get_points(measurement=measurement, tags=tags))
+    raw_acm_data_list = list(result_set.get_points(measurement=ACCELEROMETER_MEASUREMENT_NAME, tags=tags))
     raw_acm_dataframe = pd.DataFrame(raw_acm_data_list)[["time", "x_acm", "y_acm", "z_acm"]]
     raw_acm_dataframe["time"] = pd.to_datetime(raw_acm_dataframe["time"])
     raw_acm_dataframe.index = raw_acm_dataframe["time"]
@@ -64,53 +66,52 @@ def create_energy_data_points(acm_dataframe: pd.DataFrame) -> pd.DataFrame:
     :param acm_dataframe:
     :return:
     """
-    mask = acm_dataframe["time"].diff(periods=1) < "00:00:00.1"
-    diff = acm_dataframe[mask].diff(periods=1).drop(columns=["time"])
-    squared_df = diff ** 2
-    sum_df = squared_df.sum(axis=1).to_frame()
-    sum_df = sum_df.rename(columns={0: "squared_diff_sum"})
-    sum_df.index.name = "timestamp"
-    return sum_df
+    boolean_mask = acm_dataframe["time"].diff(periods=1) < "00:00:00.1"
+    consecutive_differences_dataframe = acm_dataframe.diff(periods=1)[boolean_mask].drop(["time"], axis=1)
+    squared_differences_dataframe = consecutive_differences_dataframe ** 2
+
+    triaxial_sum_dataframe = squared_differences_dataframe.sum(axis=1).to_frame()
+    triaxial_sum_dataframe = triaxial_sum_dataframe.rename(columns={0: "squared_diff_sum"})
+
+    acm_dataframe_index = acm_dataframe[boolean_mask]["time"]
+    triaxial_sum_dataframe.index = acm_dataframe_index
+    triaxial_sum_dataframe.index.name = "timestamp"
+    return triaxial_sum_dataframe
 
 
-def write_energy_points_for_user(user: str) -> bool:
+def write_energy_points_for_user(user: str, first_day: int, last_day: int) -> bool:
     """
     TODO
-
     :param user:
+    :param first_day:
+    :param last_day:
     :return:
     """
     print("[Creation of features] user {}".format(user))
 
-    for day in range(120, 160):
-        # Extract raw data from InfluxDB (for N last days)
-        query = "SELECT * FROM {} WHERE \"user\" = '".format(MEASUREMENT) + user + "' and time < now() - {}d and time > now() - {}d".format(day, day+1)
-        result_query = CLIENT.query(query)
+    for day in range(first_day, last_day):
+        # Extract raw data from InfluxDB for D-day
+        query = "SELECT * FROM {} WHERE \"user\" = '{}' and time < now() - {}d and time > now() - {}d".format(MEASUREMENT, user, day, day+1)
+        extracted_data_result_set = CLIENT.query(query)
 
-        # Transform data in a pandas Dataframe
+        # Transform InfluxDB ResultSet in pandas Dataframe
         tags = {"user": user}
         try:
-            raw_acm_dataframe = transform_acm_result_set_into_dataframe(result_query, MEASUREMENT, tags)
+            raw_acm_dataframe = transform_acm_result_set_into_dataframe(extracted_data_result_set, tags)
         except KeyError:
+            # If there is no data for D-day, skip processing
             continue
-        # Create energy features data points
-        sum_df = create_energy_data_points(raw_acm_dataframe)
-        print(sum_df.shape)
-        row_nb = len(sum_df)
-        if row_nb >= 1000000:
-            for chunk in np.array_split(sum_df, 200):
-                # Write new features into influxdb
-                tags = {USER_PARAM_NAME: user}
-                DF_CLIENT.write_points(chunk, measurement=MEASUREMENT, tags=tags, protocol="json")
-        elif 1000000 > row_nb > 100000:
-            for chunk in np.array_split(sum_df, 20):
-                # Write new features into influxdb
-                tags = {USER_PARAM_NAME: user}
-                DF_CLIENT.write_points(chunk, measurement=MEASUREMENT, tags=tags, protocol="json")
-        else:
-            # Write new features into influxdb
+
+        # Create triaxial sum data points
+        triaxial_sum_dataframe = create_energy_data_points(raw_acm_dataframe)
+
+        # Write new features into influxdb
+        # Chunk dataframe for influxdb performance issues
+        chunk_nb = math.ceil(len(triaxial_sum_dataframe) / 5000)
+        for chunk in np.array_split(triaxial_sum_dataframe, chunk_nb):
+            print("chunk shape : {}".format(chunk.shape))
             tags = {USER_PARAM_NAME: user}
-            DF_CLIENT.write_points(sum_df, measurement=MEASUREMENT, tags=tags, protocol="json")
+            DF_CLIENT.write_points(chunk, measurement=MEASUREMENT, tags=tags, protocol="json")
 
     print("[Writing process Done]")
     return True
@@ -118,11 +119,10 @@ def write_energy_points_for_user(user: str) -> bool:
 
 if __name__ == "__main__":
 
-    run_path = os.path.dirname(os.path.abspath(__file__))
-
     config = configparser.ConfigParser()
-    config.read(run_path + '/config.conf')
+    config.read('config.conf')
 
+    # Useful Influx client constants
     influxdb_client_constants = config["Influxdb Client"]
     DB_NAME = influxdb_client_constants["database_name"]
     HOST = influxdb_client_constants["host"]
@@ -130,20 +130,16 @@ if __name__ == "__main__":
     USER = influxdb_client_constants["user"]
     PASSWORD = influxdb_client_constants["password"]
 
-    MEASUREMENT = "MotionAccelerometer"
     # see InfluxDB Python API for more information
     # https://influxdb-python.readthedocs.io/en/latest/api-documentation.html
     CLIENT = InfluxDBClient(host=HOST, port=PORT, username=USER, password=PASSWORD,
                             database=DB_NAME)
-
-    # Create influxDB dataframe client
     DF_CLIENT = DataFrameClient(host=HOST, port=PORT, username=USER, password=PASSWORD,
                                 database=DB_NAME)
     print("[Client created]")
 
-    # Get list of all users
+    MEASUREMENT = "MotionAccelerometer"
     user_list = get_user_list(CLIENT, MEASUREMENT)
-    # user_list = ["2b3d7a98-a1fb-47ad-9705-4a91d448da9b"]
 
     for user in user_list:
-        write_energy_points_for_user(user)
+        write_energy_points_for_user(user=user, first_day=120, last_day=160)
